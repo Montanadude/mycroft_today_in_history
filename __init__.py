5a7320eae79c0befc497167c000bfb1f6e91c0c6
EC2 Default User <ec2-user@ip-172-31-23-83.us-east-2.compute.internal>
2019-09-01 22:29:27 +0000

    First commit

---
+++ __init__.py
@@ -0,0 +1,43 @@
+# Copyright 2016 Mycroft AI, Inc.
+#
+# This file is part of Mycroft Core.
+#
+# Mycroft Core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mycroft Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+
+from adapt.intent import IntentBuilder
+
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+
+__author__ = 'montanaguy'
+
+LOGGER = getLogger(__name__)
+
+
+class TodayInHistorySkill(MycroftSkill):
+    def __init__(self):
+        super(TodayInHistorySkill, self).__init__(name="TodayInHistorySkill")
+#        
+
+    def initialize(self):
+        self.load_data_files(dirname(__file__))
+        
+        random_event_intent = IntentBuilder("RandomEventIntent").\
+           require("RandomEventKeyword").buid()
+        self.register_intent(random_event_intent, self.handle_random_event_intent)
+
+
+   def handle_random_event_intent(self, message);
+def create_skill():
+    return TodayInHistorySkill()
